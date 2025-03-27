7e38f81cc339a2589dffb8ddba224cb5d48ff0cb
Ideadly-Skies <Obie.kal22@gmail.com>
2025-03-27 21:54:10 +0700

    feat: declare and implement ampersand and pointer showcase

---
+++ examples/declaring_pointers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    // countPtr is a pointer to an int 
+    int *countPtr;
+    
+    // define pointer and int variable
+    int *countPtr, count; 
+    
+    return (0);
+}
